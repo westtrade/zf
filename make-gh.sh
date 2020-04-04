06369bd97b14243988606ccb4d2099ad60d5388b
Popov Gennadiy <gennadiy.popov.87@yandex.ru>
2020-04-04 19:23:27 +0300

    Update gh page

---
+++ make-gh.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+find . -maxdepth 1 -type f  \( -name "entry.*" -o -name "index.html"  \) -delete
+yarn build
+mv ./dist/* .
+rm -r ./dist
+git add --all && git commit -m 'Update gh page' && git push
